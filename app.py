876019bf6be70f37fb04246795ae672c5376bd34
VilleAR <vilpuurunen@gmail.com>
2020-09-21 01:38:33 +0300

    fixed?

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:kalibri1@localhost/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
